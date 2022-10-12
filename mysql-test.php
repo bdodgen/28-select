050744a3d37a827072021787dd64aef329b1ffbc
Briana Dodgen <brianadodgen@gmail.com>
2022-10-12 11:06:58 +0200

    whatevs

--- mysql-test.php
+++ mysql-test.php
@@ -2,3 +2,10 @@
 
 require_once 'DB.php';
 require_once 'DB_functions.php';
+
+connect(
+  'localhost', // server URL
+  'world',     // database name
+  'root',      // username
+  ''           // password
+);
