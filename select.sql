57f349c35a7b0a93c90fa7cf6e603aac738216d8
Briana Dodgen <brianadodgen@gmail.com>
2022-10-12 10:59:47 +0200

    continuing classwork

---
+++ select.sql
@@ -0,0 +1,27 @@
+SELECT *
+FROM `countries`
+WHERE 1; -- "I thought about this, I did not forget WHERE"
+
+
+SELECT *
+FROM `countries`
+WHERE `population` > 20000000;
+
+
+SELECT *
+FROM `countries`
+WHERE `population` > 20000000
+ORDER BY `population`;
+
+
+SELECT *
+FROM `countries`
+WHERE `population` > 20000000
+ORDER BY `population`
+LIMIT 10;
+
+SELECT *
+FROM `countries`
+WHERE `population` > 20000000
+ORDER BY `population`
+LIMIT 10;
